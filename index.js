4f1039b5bc956f5128ee182b46d04a816e2d9ff2
Vitaliy Gerassimov <102645214+VitaliyDeveloper@users.noreply.github.com>
2023-01-05 09:57:36 +0600

    3

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ document.getElementById("reset").onclick = reset;
 
 canvas.width = 500;
 canvas.height = 500;
-canvas.style.border = "1px solid #000";
+canvas.style.border = "3px solid #fff";
 canvas.id = "cl";
 
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
 
 let mas = [];
 let counter = 0;
@@ -35,6 +34,7 @@ function goLife() {
     m = 50;
   for (let i = 0; i < m; i++) {
     mas[i] = [];
+
     for (let j = 0; j < n; j++) {
       mas[i][j] = 0;
     }
@@ -48,6 +48,9 @@ function drawField() {
   for (let i = 0; i < 50; i++) {
     for (let j = 0; j < 50; j++) {
       if (mas[i][j] === 1) {
+        // ctx.fillStyle = generateColor();
+        ctx.fillStyle = getRamdomColor();
+
         ctx.fillRect(j * 10, i * 10, 10, 10);
       }
     }
@@ -75,9 +78,9 @@ function startLife() {
   mas = mas2;
   drawField();
   counter++;
-  generationCount.innerText = `COUNTER: ${counter++}`;
   timer = setTimeout(startLife, 300);
 }
+generationCount.innerText = `COUNTER: ${counter++}`;
 
 function fpm(i) {
   if (i === 0) return 50;
@@ -91,6 +94,34 @@ function fpp(i) {
 
 function reset() {
   window.location.reload();
+}
+
+// function generateColor() {
+//   const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+//   let code = "";
+//   for (let i = 0; i < 6; i++) {
+//     code += hexArray[Math.floor(Math.random() * 16)];
+//   }
+//   return `#${code}`;
+// }
+
+function getRamdomColor() {
+  const colors = [
+    // "#BFFF00",
+    // "#FFD700",
+    // "#EB6123",
+    // "#DF73FF",
+    // "#4B0082",
+    // "#FBAED2",
+    // "#545AA7",
+    // "#15F2FD",
+    "#FFF832",
+    "#6984FF",
+    // "#FF1482",
+    // "#52FF46",
+  ];
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
 }
 
 root.appendChild(generationCount);
